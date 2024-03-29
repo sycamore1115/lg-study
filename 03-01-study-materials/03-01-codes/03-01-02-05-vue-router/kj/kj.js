2bcec1945253c9dc346072e0b95f1acad5109120
吴彤 <phoeny.wu@logwirecloud.com>
2024-03-29 14:16:37 +0800

    update

--- 03-01-study-materials/03-01-codes/03-01-02-05-vue-router/kj/kj.js
+++ 03-01-study-materials/03-01-codes/03-01-02-05-vue-router/kj/kj.js
@@ -1,4 +1,4 @@
-// 大家首先可以随便找一个 Vue 项目，看下其中 VueRouter 的使用，大概分为以下：
+// 大家首先可以随便找一个 Vue 项目，看下其中 VueRouter 的使用，大概分为以下几步：
 
 // 1. VueRouter 是一个 Vue 插件，需要使用 Vue.use(VueRouter) 注册
 
@@ -42,27 +42,16 @@ class VueRouter {
 
 // 3. 创建 Vue 根实例时传入 VueRouter 实例
 // new Vue({ router, render: h => h(App) }).$mount('#app')  
-// 创建 Vue 根实例时传入 router 实例，会往所有 Vue 实例上添加 $route 和 $router 属性。
-// 我们不会在 Vue 中的代码里实现添加 $router 的代码，所以可以在 install 方法里就将这部分代码写上，然后等待创建 Vue 根实例的时候再执行。
-// 这要利用 Vue 给我们提供的钩子函数，在创建 Vue 实例的时候，会执行 beforeCreate 钩子函数
-// 我们可以使用 Vue.mixin 进行全局混入，这个会影响注册之后所有创建的 Vue 实例
-class VueRouter {
-    static install(Vue) {     
-        if (VueRouter.install.installed) return
-        VueRouter.install.installed = true  
-        _Vue = Vue
-        _Vue.mixin({
-            beforeCreate(){
-            }
-        })
-    }
-    constructor(options) {
-        this.options = options
-        this.data = _Vue.observable({ current: '/' })
-    }
-}
 
-// 
+// 创建 Vue 根实例时传入 router 实例，会往所有 Vue 实例上添加 $router 属性。
+// 我们仔细分析这句话，有三个重点：
+// a. 在 VueRouter 类中怎么写会让代码等待 Vue 创建实例的时候再执行？
+//    这要利用 Vue 给我们提供的钩子函数，在创建 Vue 实例的时候，会执行 beforeCreate 钩子函数
+//    我们可以使用 Vue.mixin 进行全局混入，这个会影响注册之后所有创建的 Vue 实例
+// b. 怎么判断是否是 Vue 根实例的创建
+//    只有在创建根实例的时候，才会传入 router 选项
+// c. 怎么往所有 Vue 实例上添加 $router 属性
+//    往 Vue 的原型上添加，那以后创建的所有 Vue 实例上就会有这个 $router 属性
 class VueRouter {
     static install(Vue) {     
         if (VueRouter.install.installed) return
@@ -70,6 +59,7 @@ class VueRouter {
         _Vue = Vue
         _Vue.mixin({
             beforeCreate(){
+                // 这段代码写在 beforeCreate 中，this 的值指向的就是 Vue 实例，那么 this.$options.router 就可以取到传入的 router 选项。
                 if(this.$options.router){
                     _Vue.prototype.$router = this.$options.router
                 }
@@ -82,9 +72,8 @@ class VueRouter {
     }
 }
 
-
-
-// 4. <router-link to="/">Index</router-link> 
+// 4. 创建 router-link 和 router-view 两个组件
+//    <router-link to="/">Index</router-link> 
 //    <router-view/>
 class VueRouter {
     static install(Vue) {     
