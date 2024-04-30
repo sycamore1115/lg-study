d3d9b1a2f4f4c60f878cc1894167d82541405d7f
吴彤 <phoeny.wu@logwirecloud.com>
2024-04-30 16:56:14 +0800

    update

---
+++ bli-course/03-01/vue/vue7.js
@@ -0,0 +1,144 @@
+class Dep {
+    constructor() {
+        this.subs = []
+    }
+    addSubs(sub) {
+        if(sub && sub.update) this.subs.push(sub)
+    }
+    notify() {
+        this.subs.forEach(sub => { sub.update() })
+    }
+}
+class Watcher {
+    constructor(vm, key, cb) {
+        this.cb = cb
+        Dep.target = this
+        vm[key]
+        Dep.target = null
+    }
+    update() {
+        this.cb()
+    }
+}
+class Compiler{
+    constructor(vm) {
+        this.vm = vm
+        this.compiler(vm.$el)
+    }
+    compiler(el) {
+        // 遍历 el
+        let childNodes = el.childNodes || []
+        Array.from(childNodes).forEach(node => {
+            if (this.isTextNode(node)) {
+                this.compilerText(node)
+            } else if (this.isElementNode(node)) {
+                this.compilerElement(node)
+            }
+            if (node.childNodes && node.childNodes.length) this.compiler(node)
+        })
+    }
+    compilerText(node) {
+        let value = node.textContent
+        let reg = /\{\{(.+?)\}\}/
+        console.log('value', value)
+        if (reg.test(value)) {
+            value.replace(reg, (match, p1) => {
+                let key = p1.trim()
+                node.textContent = this.vm[key]
+                new Watcher(this.vm, key, () => {
+                    node.textContent = this.vm[key]
+                })
+            })
+        }
+    }
+    compilerElement(node) {
+        // 遍历元素的属性
+        Array.from(node.attributes).forEach(attr => {
+            const attrName = attr.name 
+            if (this.isDirective(attrName)) {
+                const updateFn = this[attrName.substring(2) + 'Updater']
+                updateFn && updateFn.call(this, node, attr.value, this.vm[attr.value])
+            }
+        })
+    }
+    textUpdater(node, key, value) {
+        node.textContent = value
+        new Watcher(this.vm, key, () => {
+            node.textContent = this.vm[key]
+        })
+    }
+    modelUpdater(node, key, value) {
+        node.value = value
+        node.addEventListener('input', () => {
+            this.vm[key] = node.value
+        })
+        new Watcher(this.vm, key, () => {
+            node.value = this.vm[key]
+        })
+    }
+    isTextNode(node) {
+        return node.nodeType === 3
+    }
+    isElementNode(node) {
+        return node.nodeType === 1
+    }
+    isDirective(attrName) {
+        return attrName.startsWith('v-')
+    }
+}
+class Observer {
+    constructor(data) {
+        this.walk(data)
+    }
+    walk(data) {
+        if (!data || typeof data !== 'object') return
+        Object.keys(data).forEach(key => {
+            this.defineReactive(data, key)
+        })
+    }
+    defineReactive(data, key) {
+        let dep = new Dep()
+        let value = data[key]
+        this.walk(value)
+        const self = this
+        Object.defineProperty(data, key, {
+            configurable: true,
+            enumerable: true,
+            get() {
+                Dep.target && dep.addSubs(Dep.target)
+                return value
+            },
+            set(newValue) {
+                if (newValue === value) return
+                value = newValue
+                self.walk(newValue)
+                dep.notify()
+            }
+        })
+    }
+}
+class Vue {
+    constructor(options) {
+        this.$options = options || {}
+        this.$data = options.data || {}
+        this.$el = typeof options.el === 'string' ? document.querySelector(options.el) : options.el 
+        this._proxyData(this.$data)
+        new Observer(this.$data)
+        new Compiler(this)
+    }
+    _proxyData(data) {
+        Object.keys(data).forEach(key => {
+            Object.defineProperty(this, key, {
+                configurable: true,
+                configurable: true,
+                get() {
+                    return data[key]
+                },
+                set(newValue) {
+                    if (newValue === data[key]) return
+                    data[key] = newValue
+                }
+            })
+        })
+    }
+}
--- bli-course/03-02/my/self3.js
+++ bli-course/03-02/my/self3.js
@@ -2,4 +2,37 @@
 当我们看 vue 源码的时候，会有很多标红或者不显示高亮的地方，这需要我们设置一下 vscode:
 1. 在 vscode 的 settings.json 中添加 "javascript.validate.enable": false,
 2. 添加 vscode 插件: Babel JavaScript
+
+我们先看一看 src/core/index.js 文件
+initGlobalAPI :  Vue 的构造函数添加了很多静态属性和方法
+这个方法在 src\core\global-api\index.js，我们看下这个方法做了什么：
+1. 初始化 config 成员
+   在 src\platforms\web\runtime\index.js(平台相关) 中给config上添加了相应的方法
+2. 设置了 util 成员，其中的方法是内部使用的，不建议我们去调用的
+3. 定义了 set、delete、nextTick、observable 方法
+4. 初始化 Vue.options 对象
+   设置其为原型为 null 的对象
+   向上添加 components/directives/filters 属性，用来存储全局的组件，指令和过滤器
+   将 Vue 的构造函数存储到 options 对象的 _base 属性上
+   将 builtInComponents 上所有的成员复制到 options 的 components 属性上，也就是注册全局组件KeepAlive
+5. 给 Vue 注册了静态方法
+   initUse：注册了 Vue.use 方法，用于注册插件
+   Vue.use/Vue.mixin/Vue.extend/Vue.component/Vue.directive/Vue.filter
 **/
+Vue.use = function (plugin: Function | Object) {
+    const installedPlugins = (this._installedPlugins || (this._installedPlugins = []))
+    if (installedPlugins.indexOf(plugin) > -1) {
+        return this
+    }
+
+    // additional parameters
+    const args = toArray(arguments, 1)
+    args.unshift(this)
+    if (typeof plugin.install === 'function') {
+        plugin.install.apply(plugin, args)
+    } else if (typeof plugin === 'function') {
+        plugin.apply(null, args)
+    }
+    installedPlugins.push(plugin)
+    return this
+}
